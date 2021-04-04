ae32641004265433a54186c9b8ee6716c05caee3
Hadi Abaza <hadiabaza1@gmail.com>
2021-04-04 23:15:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = `Hello, ${Neil deGrasse Tyson}!`
+var specialGuest = `Hello, ${specialGuest}!`
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
